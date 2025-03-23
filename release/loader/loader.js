b6d3fe96cbc22df1707c61e2c3d06f26e8c72123
Jonah <jonah.ahvonen@gmail.com>
2025-03-23 02:48:31 +0200

    Fix loader

--- release/loader/loader.js
+++ release/loader/loader.js
@@ -1,9 +1,9 @@
 javascript:(function() {
-    if(hypersynergism in window) {
+    if('hypersynergism' in window) {
         alert('Hypersynergism is already loaded on the page, please refresh if you want to reload the mod');
         return;
     };
-    
+
     const scriptSrc = `https://cdn.jsdelivr.net/gh/ahvonenj/synergism-hypersynergy@latest/release/mod/hypersynergism_release.js?r=${Math.floor(Math.random() * 1000000)}`;
     const script = document.createElement('script');
     script.src = scriptSrc;
